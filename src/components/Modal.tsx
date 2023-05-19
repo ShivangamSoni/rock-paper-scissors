803040fca11c8d982f7bf51c1611997e002af5af
Shivangam Soni <shivangamsoni99@gmail.com>
2023-05-19 18:25:05 +0530

    Add: Reusable Modal

---
+++ src/components/Modal.tsx
@@ -0,0 +1,43 @@
+import { ReactNode } from 'react';
+import { createPortal } from 'react-dom';
+
+export default function Modal({
+  open,
+  title,
+  children,
+  onClose,
+}: {
+  open: boolean;
+  title: string;
+  children: ReactNode;
+  onClose: () => void;
+}) {
+  if (!open) {
+    return null;
+  }
+
+  return createPortal(
+    <div className="fixed inset-0 bg-black bg-opacity-30 flex items-center justify-center text-neutral-dark font-semibold font-primary">
+      <div className="grid gap-12 bg-white p-12 w-full max-w-lg relative rounded-xl">
+        <header>
+          <h2 className="uppercase tracking-wide text-3xl font-bold">
+            {title}
+          </h2>
+        </header>
+
+        <div>{children}</div>
+
+        <footer className="absolute top-12 right-12">
+          <button
+            className="flex items-center justify-center border-none outline outline-neutral-outline outline-0 p-1.5 rounded-full hover:outline-2 focus-visible:outline-2"
+            onClick={onClose}
+          >
+            <span className="sr-only">Close</span>
+            <img src="/assets/icon/icon-close.svg" alt="" />
+          </button>
+        </footer>
+      </div>
+    </div>,
+    document.querySelector('#modal') as HTMLDivElement,
+  );
+}
