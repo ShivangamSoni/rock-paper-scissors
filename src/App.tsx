efacdd8c94a3f3971bc15c6b4c75e5608ca5f77a
Shivangam Soni <shivangamsoni99@gmail.com>
2023-05-19 17:19:05 +0530

    Add: Custom Tailwind Theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 export default function App() {
   return (
-    <div className="min-h-screen bg-slate-900 text-white">
+    <div className="min-h-screen bg-main text-white">
       <h1 className="text-3xl font-semibold font-mono uppercase">App</h1>
+      <div className="flex items-center justify-start gap-4">
+        <button className="w-24 h-24 flex items-center justify-center text-xl bg-rock">
+          <img src="/assets/icon/icon-rock.svg" alt="" />
+        </button>
+        <button className="w-24 h-24 flex items-center justify-center text-xl bg-paper">
+          <img src="/assets/icon/icon-paper.svg" alt="" />
+        </button>
+        <button className="w-24 h-24 flex items-center justify-center text-xl bg-scissors">
+          <img src="/assets/icon/icon-scissors.svg" alt="" />
+        </button>
+        <button className="w-24 h-24 flex items-center justify-center text-xl bg-lizard">
+          <img src="/assets/icon/icon-lizard.svg" alt="" />
+        </button>
+        <button className="w-24 h-24 flex items-center justify-center text-xl bg-spock">
+          <img src="/assets/icon/icon-spock.svg" alt="" />
+        </button>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        neutral: {
+          dark: 'hsl(229, 25%, 31%)',
+          score: 'hsl(229, 64%, 46%)',
+          outline: 'hsl(217, 16%, 45%)',
+        },
+      },
+      backgroundImage: {
+        main: 'radial-gradient(circle at 50% 0%, hsl(214, 47%, 23%), hsl(237, 49%, 15%))',
+        scissors: 'linear-gradient(hsl(39, 89%, 49%), hsl(40, 84%, 53%))',
+        paper: 'linear-gradient(hsl(230, 89%, 62%), hsl(230, 89%, 65%))',
+        rock: 'linear-gradient(hsl(349, 71%, 52%), hsl(349, 70%, 56%))',
+        lizard: 'linear-gradient(hsl(261, 73%, 60%), hsl(261, 72%, 63%))',
+        spock: 'linear-gradient(hsl(189, 59%, 53%), hsl(189, 58%, 57%))',
+      },
+    },
   },
   plugins: [],
 };
