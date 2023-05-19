841ca9c8c0475b59c0717db08264364194da0a19
Shivangam Soni <shivangamsoni99@gmail.com>
2023-05-19 16:52:37 +0530

    Initialize React Project with Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-slate-900 text-white">
+      <h1 className="text-3xl font-semibold font-mono uppercase">App</h1>
+    </div>
+  );
+}
