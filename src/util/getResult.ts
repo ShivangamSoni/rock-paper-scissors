dbf1c31198825c4c820f9cb160b4f0989b286157
Shivangam Soni <shivangamsoni99@gmail.com>
2023-05-20 17:04:08 +0530

    Add gerResult Utility

---
+++ src/util/getResult.ts
@@ -0,0 +1,18 @@
+import { Choice, Result } from '@customTypes/Game';
+
+export function getResult(
+  playerChoice: Choice,
+  computerChoice: Choice,
+): Result {
+  if (playerChoice === computerChoice) {
+    return 'tie';
+  } else if (
+    (playerChoice === 'rock' && computerChoice === 'scissors') ||
+    (playerChoice === 'paper' && computerChoice === 'rock') ||
+    (playerChoice === 'scissors' && computerChoice === 'paper')
+  ) {
+    return 'win';
+  } else {
+    return 'lose';
+  }
+}
